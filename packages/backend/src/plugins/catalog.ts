f4ffb3cc29292592e6050ff1a7e18e0d865934ef
Luis Gomez <l.gomezvazquez-ext@ubitransport.com>
2023-02-14 10:14:37 +0100

    Let's go for the next step

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -2,12 +2,32 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
+import { GithubOrgEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
+import { GithubOrgReaderProcessor } from '@backstage/plugin-catalog-backend-module-github';
+
+
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
-  builder.addProcessor(new ScaffolderEntitiesProcessor());
+  // The org URL below needs to match a configured integrations.github entry
+  // specified in your app-config.
+  builder.addEntityProvider(
+    GithubOrgEntityProvider.fromConfig(env.config, {
+      id: 'production',
+      orgUrl: 'https://github.com/ubiportal',
+      logger: env.logger,
+      schedule: env.scheduler.createScheduledTaskRunner({
+        frequency: { minutes: 60 },
+        timeout: { minutes: 15 },
+      }),
+    }),
+  );
+  // builder.addProcessor(new ScaffolderEntitiesProcessor());
+  builder.addProcessor(
+    GithubOrgReaderProcessor.fromConfig(env.config, { logger: env.logger }),
+  );
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
